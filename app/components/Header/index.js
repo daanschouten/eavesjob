bf4ebdbec72d6981c97e7125120ae6e96caf4ad2
Daan Schouten <daanjanschouten@gmail.com>
2018-02-12 16:59:43 +0000

    simplified header functionality

--- app/components/Header/index.js
+++ app/components/Header/index.js
@@ -2,53 +2,19 @@ const React = require('react');
 const PropTypes = require('prop-types');
 const { NavLink } = require('react-router-dom');
 
-class MenuLoggedIn extends React.Component {
-  render() {
-    return (
-      <div id="menu">
+export default function Header(props) {
+  return (
+    <header>
+      <div id="logo">
+        <NavLink exact to="/">Home</NavLink>
+        <NavLink to="/browse">Browse</NavLink>
         <NavLink to="/about">About</NavLink>
-        <NavLink to="/support">Support</NavLink>
         <NavLink to="/contact">Contact</NavLink>
+      </div>
+      <div id="menu">
+        <NavLink to="/support">Support</NavLink>
         <NavLink to="/profile">My Profile</NavLink>
       </div>
-    )
-  }
-}
-
-class MenuNotLoggedIn extends React.Component {
-  render() {
-    return (
-      <div id="menu">
-        <NavLink to="/about">About</NavLink>
-        <NavLink to="/support">Support</NavLink>
-        <NavLink to="/contact">Contact</NavLink>
-        <NavLink to="/register">Sign Up</NavLink>
-        <NavLink to="/login">Login</NavLink>
-      </div>
-    )
-  }
-}
-
-const Menu = function(props) {
-  if (props.user._id) {
-    return <MenuLoggedIn/>;
-  }
-  return <MenuNotLoggedIn/>;
-}
-
-export default class Header extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-  render() {
-    return (
-      <header>
-        <div id="logo">
-          <NavLink exact to="/">Home</NavLink>
-          <NavLink to="/browse">Browse</NavLink>
-        </div>
-        <Menu user={this.props.user}/>
-      </header>
-    )
-  }
+    </header>
+  )
 }
