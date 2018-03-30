ea5ef530c8c9e46703a8baec7326f7f2bf52849b
Daan Schouten <daanjanschouten@gmail.com>
2018-03-30 16:57:38 +0100

    files before switching to pm2

--- api_info.js
+++ api_info.js
@@ -1,5 +1,5 @@
-const API_ADDRESS = "http://localhost";
-const API_PORT = 3000;
+const API_ADDRESS = "https://api.eavesjob.com";
+const API_PORT = 443;
 
 const API_FULL = `${API_ADDRESS}:${API_PORT}`;
 
