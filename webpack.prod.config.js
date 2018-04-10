c74a536320b5882997b32bd97ead7fdffc856cab
Daan Schouten <daanjanschouten@gmail.com>
2018-04-10 19:20:41 +0100

    temporarily removed gzip compression

--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -71,13 +71,13 @@ module.exports = {
             comments: false
           }
         }),
-        new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/),
-        new CompressionPlugin({
-          asset: "[path].gz[query]",
-          algorithm: "gzip",
-          test: /\.js$|\.css$|\.html$/,
-          threshold: 10240,
-          minRatio: 0.8
-        })
+        new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/)
+        // new CompressionPlugin({
+        //   asset: "[path].gz[query]",
+        //   algorithm: "gzip",
+        //   test: /\.js$|\.css$|\.html$/,
+        //   threshold: 10240,
+        //   minRatio: 0.8
+        // })
     ]
 };
